5f0d2d68210c5ca439c5b91a49c32003c27d9a0f
AlexandreClose <alexandre.close@gmail.com>
2016-12-09 17:50:20 +0100

    Initial work

---
+++ src/java/fr/paris/lutece/plugins/crm/modules/favorites/service/provider/DemandTypeFavoritesProvider.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2002-2014, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.crm.modules.favorites.service.provider;
+
+import fr.paris.lutece.plugins.crm.business.demand.DemandType;
+import fr.paris.lutece.plugins.crm.service.demand.DemandTypeService;
+import fr.paris.lutece.plugins.mydashboard.modules.favorites.business.Favorite;
+import fr.paris.lutece.plugins.mydashboard.modules.favorites.service.provider.IFavoriteProvider;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemandTypeFavoritesProvider implements IFavoriteProvider
+{
+    private static final String CRM_DEMAND_TYPE_FAVORITES_PROVIDER_NAME = "CRM_DEMAND_TYPE_FAVORITES_PROVIDER";
+    
+    @Override
+    public List<Favorite> getProvideFavorites( )
+    {
+        List<Favorite> listFavorites = new ArrayList<Favorite>( );
+        for ( DemandType demandType : DemandTypeService.getService( ).findAll( ) )
+        {
+            Favorite favorite = new Favorite( );
+            favorite.setIsActivated( demandType.isOpen( ) );
+            favorite.setLabel( demandType.getLabel( ) );
+            favorite.setProviderName( CRM_DEMAND_TYPE_FAVORITES_PROVIDER_NAME );
+            favorite.setRemoteId( Integer.toString( demandType.getIdDemandType( ) ) );
+            favorite.setUrl( demandType.getUrlResource( ) );
+            listFavorites.add( favorite );
+        }
+        return listFavorites;
+    }
+    
+    @Override
+    public String getFavoriteProviderName( )
+    {
+        return CRM_DEMAND_TYPE_FAVORITES_PROVIDER_NAME;
+    }
+}
